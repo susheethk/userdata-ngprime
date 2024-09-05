10028fd679566bc4343a2ce7f09ada111b7d5cc9
susheethk <susheethk@users.noreply.github.com>
2024-09-05 22:56:49 +0530

    Added routes to buttons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { PagetwoComponent } from './pagetwo/pagetwo.component';
+import { PagethreeComponent } from './pagethree/pagethree.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'mainpage', component: MainpageComponent},
+    {path: 'pagetwo', component: PagetwoComponent},
+    {path: 'pagethree', component: PagethreeComponent},
+    {path: 'navbar', component: NavbarComponent},
+    { path: '', redirectTo: '/navbar', pathMatch: 'full' }, 
+];
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
+// import { RouterLink } from '@angular/router';
+// import { RouterLinkActive } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [SidebarModule, ButtonModule,CommonModule],
+  imports: [SidebarModule, ButtonModule,CommonModule,RouterModule],
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']  
 })
